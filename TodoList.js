feae366c05df28890de71f490cec7451b371d27a
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-11-10 01:29:18 +0900

    validation 추가

--- TodoList.js
+++ TodoList.js
@@ -1,22 +1,45 @@
-  
+import {
+    useNewKeyword,
+    useArrayState,
+    checkTarget,
+    checkTypes,
+} from './validation.js'
+    
+
 // DOM 접근을 최소화
 // DOM을 변경하는 곳은 하나의 패턴으로만 해야할 것
 export default function TodoList($target, initalState) {
   this.state = initalState;
   this.$target = $target;
 
+  this.validation = (state) => {
+    useNewKeyword(this)
+    useArrayState(state)
+    checkTarget(this.$target)
+    checkTypes(
+      state,
+      ({ text, isCompleted }) =>
+        typeof text === 'string' && typeof isCompleted === 'boolean'
+    )
+  }
+
   this.render = () => {
-    this.$target.innerHTML = `<ul>${this.state
-      // todo { text, isCompleted }
-      .map(({ text, isCompleted }) =>
-        isCompleted ? `<li><s>${text}</s></li>` : `<li>${text}</li>`
-      )
-      .join("")}</ul>`;
-  };
+    const htmlString =
+      this.state.length > 0
+        ? `<ul>${this.state
+            .map(({ text, isCompleted }) =>
+              isCompleted ? `<li><s>${text}</s></li>` : `<li>${text}</li>`
+            )
+            .join('')}</ul>`
+        : ''
+    this.$target.innerHTML = htmlString
+  }
 
   this.setState = (nextState) => {
-    this.state = nextState;
-    this.render();
-  };
-  this.render();
+    this.validation(nextState)
+    this.state = nextState
+    this.render()
+  }
+  this.validation(this.state)
+  this.render()
 }
--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-  
-import TodoList from "./TodoList.js";
+import TodoList from './TodoList.js'
 
 const data = [
   {
-    text: "JS 공부하기",
+    text: 'JS 공부하기',
     isCompleted: true,
   },
   {
-    text: "JS 복습하기",
+    text: 'JS 복습하기',
     isCompleted: true,
   },
-];
+]
 
-const todoList1 = new TodoList(document.querySelector("#todo-list"), data);
+try {
+  const todoList1 = new TodoList(document.querySelector('#todo-list'), data)
 
-// 상태 변경
-todoList1.setState([
-  ...todoList1.state,
-  {
-    text: "EVIL!!! 😎",
-    isCompleted: false,
-  },
-]);
-
-document.querySelector("#todo-change").addEventListener("click", () => {
+  // 상태 변경
   todoList1.setState([
     ...todoList1.state,
     {
-      text: "Good!!! 😍",
+      text: 'EVIL!!! 😎',
       isCompleted: false,
     },
-  ]);
-});
+  ])
+
+  document.querySelector('#todo-change').addEventListener('click', () => {
+    todoList1.setState([
+      ...todoList1.state,
+      {
+        text: 'Good!!! 😍',
+        isCompleted: false,
+      },
+    ])
+  })
+} catch (e) {
+  document.querySelector('#errors').innerHTML = e.message
+}
